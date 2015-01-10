5588f06c03e223fe320e76683a8e94e026f6891f
Ed Eliot <me@ejeliot.com>
2015-01-10 22:06:04 +0100

    added additional functionality and improved readme

--- README.txt
+++ README.txt
@@ -1,9 +1,94 @@
-A simple class for returning ISO3 country codes and names. Optionally filter countries out of the list and/or move countries to the top of the list. Assumed it'll be used in something like an HTML select element.
+# IsoCountries
 
-Features:
+A simple class for returning ISO2 and ISO3 country codes and names. Great for populating HTML drop down country lists.
 
-* Get all country codes and names
-* Filter out specific countries from returned list
-* Move specific countries to top of returned list
-* Return list of EU member states
-* Return list by continent
+## Features
+
+* Complete ISO2/ISO3 lists with mappings to the corresponding country names
+* Get mappings for the EU, Europe, North America, South America, Africa, Asia & Oceania
+* Filter out countries you don't want
+* Move preferred countries to the top ofthe returned list
+
+## Installation
+
+1. Install Composer (assuming you haven't already got it):
+
+    curl -sS https://getcomposer.org/installer | php
+    mv composer.phar /usr/local/bin/composer
+
+2. Add IsoCountries to your composer.json file
+
+    {
+        "repositories": [
+            {
+                "type": "git",
+                "url": "https://github.com/brightfishsoftware/isocountries"
+            }
+        ],
+        "require": {
+            "brightfishsoftware/isocountries": "dev-master"
+        }
+    }
+
+3. Run composer install
+
+## Examples
+
+### Get All Countries
+
+    use BrightfishSoftware\IsoCountries;
+
+    $countries = new IsoCountries;
+    print_r($countries->get()); // as ISO3
+    print_r($countries->get(IsoCountries::RETURN_AS_ISO2)); // as ISO2
+
+### Move Country To Top
+
+    use BrightfishSoftware\IsoCountries;
+
+    $countries = new IsoCountries;
+    print_r($countries->toTop('GBR')->get());
+
+### Exclude Countries
+
+    use BrightfishSoftware\IsoCountries;
+
+    $countries = new IsoCountries;
+    print_r($countries->exclude(['AFG', 'ALA', 'ALB'])->get());
+
+### Get All EU Member States
+
+    use BrightfishSoftware\IsoCountries;
+
+    $countries = new IsoCountries;
+    print_r($countries->getEu()); // all EU
+    print_r($countries->toTop('GBR')->getEu()); // all EU but move GBR to top
+
+## License
+
+Copyright (c) 2014, Brightfish Software/Ed Eliot
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project.
--- src/IsoCountries.php
+++ src/IsoCountries.php
@@ -1,11 +1,17 @@
 <?php
+namespace BrightfishSoftware;
+
+use Exception;
 /**
- * Class Iso3Countries
+ * Class IsoCountries
  * Author: Ed Eliot
  */
-class Iso3Countries {
+class IsoCountries {
+	const RETURN_AS_ISO2 = 1;
+	const RETURN_AS_ISO3 = 2;
+
 	/** @var array */
-	protected $complete = array(
+	protected $completeIso3 = [
 		'AFG' => 'Afghanistan',
 		'ALA' => 'Åland Islands',
 		'ALB' => 'Albania',
@@ -255,36 +261,36 @@ class Iso3Countries {
 		'YEM' => 'Yemen',
 		'ZMB' => 'Zambia',
 		'ZWE' => 'Zimbabwe'
-	);
+	];
 	/** @var array */
-	protected $euComplete = array(
+	protected $euCompleteIso3 = [
 		'AUT', 'BEL', 'BGR', 'HRV', 'CYP', 'CZE', 'DNK', 'EST', 'FIN', 'FRA',
 		'DEU', 'GRC', 'HUN', 'IRL', 'ITA', 'LVA', 'LTU', 'LUX', 'MLT', 'NLD',
 		'POL', 'PRT', 'ROU', 'SVK', 'SVN', 'ESP', 'SWE', 'GBR'
-	);
+	];
 	/** @var array */
-	protected $europeComplete = array(
+	protected $europeCompleteIso3 = [
 		'ALB', 'AND', 'AUT', 'BLR', 'BEL', 'BIH', 'BGR', 'HRV', 'CYP', 'CZE',
 		'DNK', 'EST', 'FRO', 'FIN', 'FRA', 'DEU', 'GIB', 'GRC', 'HUN', 'ISL',
 		'IRL', 'ITA', 'LVA', 'LIE', 'LTU', 'LUX', 'MKD', 'MLT', 'MDA', 'MCO',
 		'NLD', 'NOR', 'POL', 'PRT', 'ROU', 'RUS', 'SMR', 'SRB', 'SVK', 'SVN',
 		'ESP', 'SWE', 'CHE', 'UKR', 'GBR', 'VAT', 'RSB', 'IMN', 'MNE'
-	);
+	];
 	/** @var array */
-	protected $northAmericaComplete = array(
+	protected $northAmericaCompleteIso3 = [
 		'AIA', 'ATG', 'ABW', 'BHS', 'BRB', 'BLZ', 'BMU', 'VGB', 'CAN', 'CYM',
 		'CRI', 'CUB', 'CUW', 'DMA', 'DOM', 'SLV', 'GRL', 'GRD', 'GLP', 'GTM',
 		'HTI', 'HND', 'JAM', 'MTQ', 'MEX', 'SPM', 'MSR', 'ANT', 'KNA', 'NIC',
 		'PAN', 'PRI', 'KNA', 'LCA', 'SPM', 'VCT', 'TTO', 'TCA', 'VIR', 'USA',
 		'SXM'
-	);
+	];
 	/** @var array */
-	protected $southAmericaComplete = array(
+	protected $southAmericaCompleteIso3 = [
 		'ARG', 'BOL', 'BRA', 'CHL', 'COL', 'ECU', 'FLK', 'GUF', 'GUY', 'PRY',
 		'PER', 'SUR', 'URY', 'VEN'
-	);
+	];
 	/** @var array */
-	protected $africaComplete = array(
+	protected $africaCompleteIso3 = [
 		'DZA', 'AGO', 'SHN', 'BEN', 'BWA', 'BFA', 'BDI', 'CMR', 'CPV', 'CAF',
 		'TCD', 'COM', 'COG', 'DJI', 'EGY', 'GNQ', 'ERI', 'ETH', 'GAB', 'GMB',
 		'GHA', 'GNB', 'GIN', 'CIV', 'KEN', 'LSO', 'LBR', 'LBY', 'MDG', 'MWI',
@@ -292,26 +298,282 @@ class Iso3Countries {
 		'REU', 'RWA', 'STP', 'SEN', 'SYC', 'SLE', 'SOM', 'ZAF', 'SHN', 'SDN',
 		'SWZ', 'TZA', 'TGO', 'TUN', 'UGA', 'COD', 'ZMB', 'TZA', 'ZWE', 'SSD',
 		'COD'
-	);
+	];
 	/** @var array */
-	protected $asiaComplete = array(
+	protected $asiaCompleteIso3 = [
 		'AFG', 'ARM', 'AZE', 'BHR', 'BGD', 'BTN', 'BRN', 'KHM', 'CHN', 'CXR',
 		'CCK', 'IOT', 'GEO', 'HKG', 'IND', 'IDN', 'IRN', 'IRQ', 'ISR', 'JPN',
 		'JOR', 'KAZ', 'PRK', 'KOR', 'KWT', 'KGZ', 'LAO', 'LBN', 'MAC', 'MYS',
 		'MDV', 'MNG', 'MMR', 'NPL', 'OMN', 'PAK', 'PHL', 'QAT', 'SAU', 'SGP',
 		'LKA', 'SYR', 'TWN', 'TJK', 'THA', 'TUR', 'TKM', 'ARE', 'UZB', 'VNM',
 		'YEM', 'PSE'
-	);
+	];
 	/** @var array */
-	protected $oceaniaComplete = array(
+	protected $oceaniaCompleteIso3 = [
 		'ASM', 'AUS', 'NZL', 'COK', 'FJI', 'PYF', 'GUM', 'KIR', 'MNP', 'MHL',
 		'FSM', 'UMI', 'NRU', 'NCL', 'NZL', 'NIU', 'NFK', 'PLW', 'PNG', 'MNP',
 		'SLB', 'TKL', 'TON', 'TUV', 'VUT', 'UMI', 'WLF', 'WSM', 'TLS'
-	);
+	];
 	/** @var array */
-	protected $exclude = array();
+	protected $iso2ToIso3 = [
+		'AF' => 'AFG',
+		'AX' => 'ALA',
+		'AL' => 'ALB',
+		'DZ' => 'DZA',
+		'AS' => 'ASM',
+		'AD' => 'AND',
+		'AO' => 'AGO',
+		'AI' => 'AIA',
+		'AQ' => 'ATA',
+		'AG' => 'ATG',
+		'AR' => 'ARG',
+		'AM' => 'ARM',
+		'AW' => 'ABW',
+		'AU' => 'AUS',
+		'AT' => 'AUT',
+		'AZ' => 'AZE',
+		'BS' => 'BHS',
+		'BH' => 'BHR',
+		'BD' => 'BGD',
+		'BB' => 'BRB',
+		'BY' => 'BLR',
+		'BE' => 'BEL',
+		'BZ' => 'BLZ',
+		'BJ' => 'BEN',
+		'BM' => 'BMU',
+		'BT' => 'BTN',
+		'BO' => 'BOL',
+		'BA' => 'BIH',
+		'BW' => 'BWA',
+		'BV' => 'BVT',
+		'BR' => 'BRA',
+		'IO' => 'IOT',
+		'BN' => 'BRN',
+		'BG' => 'BGR',
+		'BF' => 'BFA',
+		'BI' => 'BDI',
+		'KH' => 'KHM',
+		'CM' => 'CMR',
+		'CA' => 'CAN',
+		'CV' => 'CPV',
+		'KY' => 'CYM',
+		'CF' => 'CAF',
+		'TD' => 'TCD',
+		'CL' => 'CHL',
+		'CN' => 'CHN',
+		'CX' => 'CXR',
+		'CC' => 'CCK',
+		'CO' => 'COL',
+		'KM' => 'COM',
+		'CG' => 'COG',
+		'CK' => 'COK',
+		'CR' => 'CRI',
+		'CI' => 'CIV',
+		'HR' => 'HRV',
+		'CU' => 'CUB',
+		'CY' => 'CYP',
+		'CZ' => 'CZE',
+		'DK' => 'DNK',
+		'DJ' => 'DJI',
+		'DM' => 'DMA',
+		'DO' => 'DOM',
+		'EC' => 'ECU',
+		'EG' => 'EGY',
+		'SV' => 'SLV',
+		'GQ' => 'GNQ',
+		'ER' => 'ERI',
+		'EE' => 'EST',
+		'ET' => 'ETH',
+		'FK' => 'FLK',
+		'FO' => 'FRO',
+		'FJ' => 'FJI',
+		'FI' => 'FIN',
+		'FR' => 'FRA',
+		'GF' => 'GUF',
+		'PF' => 'PYF',
+		'TF' => 'ATF',
+		'GA' => 'GAB',
+		'GM' => 'GMB',
+		'GE' => 'GEO',
+		'DE' => 'DEU',
+		'GH' => 'GHA',
+		'GI' => 'GIB',
+		'GR' => 'GRC',
+		'GL' => 'GRL',
+		'GD' => 'GRD',
+		'GP' => 'GLP',
+		'GU' => 'GUM',
+		'GT' => 'GTM',
+		'GN' => 'GIN',
+		'GW' => 'GNB',
+		'GY' => 'GUY',
+		'HT' => 'HTI',
+		'HM' => 'HMD',
+		'VA' => 'VAT',
+		'HN' => 'HND',
+		'HK' => 'HKG',
+		'HU' => 'HUN',
+		'IS' => 'ISL',
+		'IN' => 'IND',
+		'ID' => 'IDN',
+		'IR' => 'IRN',
+		'IQ' => 'IRQ',
+		'IE' => 'IRL',
+		'IL' => 'ISR',
+		'IT' => 'ITA',
+		'JM' => 'JAM',
+		'JP' => 'JPN',
+		'JO' => 'JOR',
+		'KZ' => 'KAZ',
+		'KE' => 'KEN',
+		'KI' => 'KIR',
+		'KP' => 'PRK',
+		'KR' => 'KOR',
+		'KW' => 'KWT',
+		'KG' => 'KGZ',
+		'LA' => 'LAO',
+		'LV' => 'LVA',
+		'LB' => 'LBN',
+		'LS' => 'LSO',
+		'LR' => 'LBR',
+		'LY' => 'LBY',
+		'LI' => 'LIE',
+		'LT' => 'LTU',
+		'LU' => 'LUX',
+		'MO' => 'MAC',
+		'MK' => 'MKD',
+		'MG' => 'MDG',
+		'MW' => 'MWI',
+		'MY' => 'MYS',
+		'MV' => 'MDV',
+		'ML' => 'MLI',
+		'MT' => 'MLT',
+		'MH' => 'MHL',
+		'MQ' => 'MTQ',
+		'MR' => 'MRT',
+		'MU' => 'MUS',
+		'YT' => 'MYT',
+		'MX' => 'MEX',
+		'FM' => 'FSM',
+		'MD' => 'MDA',
+		'MC' => 'MCO',
+		'MN' => 'MNG',
+		'MS' => 'MSR',
+		'MA' => 'MAR',
+		'MZ' => 'MOZ',
+		'MM' => 'MMR',
+		'NA' => 'NAM',
+		'NR' => 'NRU',
+		'NP' => 'NPL',
+		'NL' => 'NLD',
+		'NC' => 'NCL',
+		'NZ' => 'NZL',
+		'NI' => 'NIC',
+		'NE' => 'NER',
+		'NG' => 'NGA',
+		'NU' => 'NIU',
+		'NF' => 'NFK',
+		'MP' => 'MNP',
+		'NO' => 'NOR',
+		'OM' => 'OMN',
+		'PK' => 'PAK',
+		'PW' => 'PLW',
+		'PA' => 'PAN',
+		'PG' => 'PNG',
+		'PY' => 'PRY',
+		'PE' => 'PER',
+		'PH' => 'PHL',
+		'PN' => 'PCN',
+		'PL' => 'POL',
+		'PT' => 'PRT',
+		'PR' => 'PRI',
+		'QA' => 'QAT',
+		'RE' => 'REU',
+		'RO' => 'ROU',
+		'RU' => 'RUS',
+		'RW' => 'RWA',
+		'KN' => 'KNA',
+		'LC' => 'LCA',
+		'VC' => 'VCT',
+		'WS' => 'WSM',
+		'SM' => 'SMR',
+		'ST' => 'STP',
+		'SA' => 'SAU',
+		'SN' => 'SEN',
+		'SC' => 'SYC',
+		'SL' => 'SLE',
+		'SG' => 'SGP',
+		'SK' => 'SVK',
+		'SI' => 'SVN',
+		'SB' => 'SLB',
+		'SO' => 'SOM',
+		'ZA' => 'ZAF',
+		'GS' => 'SGS',
+		'ES' => 'ESP',
+		'LK' => 'LKA',
+		'SH' => 'SHN',
+		'PM' => 'SPM',
+		'SD' => 'SDN',
+		'SR' => 'SUR',
+		'SJ' => 'SJM',
+		'SZ' => 'SWZ',
+		'SE' => 'SWE',
+		'CH' => 'CHE',
+		'SY' => 'SYR',
+		'TW' => 'TWN',
+		'TJ' => 'TJK',
+		'TZ' => 'TZA',
+		'TH' => 'THA',
+		'TG' => 'TGO',
+		'TK' => 'TKL',
+		'TO' => 'TON',
+		'TT' => 'TTO',
+		'TN' => 'TUN',
+		'TR' => 'TUR',
+		'TM' => 'TKM',
+		'TC' => 'TCA',
+		'TV' => 'TUV',
+		'UG' => 'UGA',
+		'UA' => 'UKR',
+		'AE' => 'ARE',
+		'GB' => 'GBR',
+		'US' => 'USA',
+		'UM' => 'UMI',
+		'UY' => 'URY',
+		'UZ' => 'UZB',
+		'VU' => 'VUT',
+		'VE' => 'VEN',
+		'VN' => 'VNM',
+		'VG' => 'VGB',
+		'VI' => 'VIR',
+		'WF' => 'WLF',
+		'EH' => 'ESH',
+		'YE' => 'YEM',
+		'ZM' => 'ZMB',
+		'ZW' => 'ZWE',
+		'BQ' => 'BES',
+		'CD' => 'COD',
+		'CW' => 'CUW',
+		'GG' => 'GGY',
+		'IM' => 'IMN',
+		'JE' => 'JEY',
+		'ME' => 'MNE',
+		'PS' => 'PSE',
+		'BL' => 'BLM',
+		'MF' => 'MAF',
+		'RS' => 'SRB',
+		'SX' => 'SXM',
+		'SS' => 'SSD',
+		'TL' => 'TLS'
+	];
 	/** @var array */
-	protected $toTop = array();
+	protected $exclude = [];
+	/** @var array */
+	protected $toTop = [];
+
+	public function __construct() {
+
+	}
 
 	/**
 	 * @param string|array $exclude
@@ -319,8 +581,11 @@ class Iso3Countries {
      */
 	public function exclude($exclude)
 	{
-		$exclude = is_array($exclude) ? $exclude : array($exclude);
+		$exclude = is_array($exclude) ? $exclude : [$exclude];
 		foreach ($exclude as $current) {
+			if (strlen($current) === 2) {
+				$current = $this->convertIso2($current);
+			}
 			$this->exclude[$current] = $current;
 		}
 		return $this;
@@ -332,8 +597,11 @@ class Iso3Countries {
      */
 	public function toTop($toTop)
 	{
-		$toTop = is_array($toTop) ? $toTop : array($toTop);
+		$toTop = is_array($toTop) ? $toTop : [$toTop];
 		foreach ($toTop as $current) {
+			if (strlen($current) === 2) {
+				$current = $this->convertIso2($current);
+			}
 			$this->toTop[$current] = $current;
 		}
 		return $this;
@@ -344,90 +612,143 @@ class Iso3Countries {
      */
 	public function reset()
 	{
-		$this->exclude = $this->toTop = array();
+		$this->exclude = $this->toTop = [];
 		return $this;
 	}
 
 	/**
+	 * @param int $as
 	 * @return array
-     */
-	public function get()
+	 */
+	public function get($as = self::RETURN_AS_ISO3)
 	{
-		$list = empty($this->exclude) ? $this->complete : array_diff_key($this->complete, $this->exclude);
+		$list = empty($this->exclude) ? $this->completeIso3 : array_diff_key($this->completeIso3, $this->exclude);
 		if (!empty($this->toTop)) {
 			$list = array_merge(array_intersect_key($list, $this->toTop), array_diff_key($list, $this->toTop));
 		}
-		return $list;
+		return $as === static::RETURN_AS_ISO2 ? $this->convertListToIso2($list) : $list;
 	}
 
 	/**
+	 * @param int $as
 	 * @return array
      */
-	public function get_eu()
+	public function getEu($as = self::RETURN_AS_ISO3)
 	{
-		return $this->get_region($this->euComplete);
+		return $this->getRegion($this->euCompleteIso3, $as);
 	}
 
 	/**
+	 * @param int $as
 	 * @return array
      */
-	public function get_europe()
+	public function getEurope($as = self::RETURN_AS_ISO3)
 	{
-		return $this->get_region($this->europeComplete);
+		return $this->getRegion($this->europeCompleteIso3, $as);
 	}
 
 	/**
+	 * @param int $as
 	 * @return array
      */
-	public function get_north_america()
+	public function getNorthAmerica($as = self::RETURN_AS_ISO3)
 	{
-		return $this->get_region($this->northAmericaComplete);
+		return $this->getRegion($this->northAmericaCompleteIso3, $as);
 	}
 
 	/**
+	 * @param int $as
 	 * @return array
      */
-	public function get_south_america()
+	public function getSouthAmerica($as = self::RETURN_AS_ISO3)
 	{
-		return $this->get_region($this->southAmericaComplete);
+		return $this->getRegion($this->southAmericaCompleteIso3, $as);
 	}
 
 	/**
+	 * @param int $as
 	 * @return array
      */
-	public function get_africa()
+	public function getAfrica($as = self::RETURN_AS_ISO3)
 	{
-		return $this->get_region($this->africaComplete);
+		return $this->getRegion($this->africaCompleteIso3, $as);
 	}
 
 	/**
+	 * @param int $as
 	 * @return array
      */
-	public function get_asia()
+	public function getAsia($as = self::RETURN_AS_ISO3)
 	{
-		return $this->get_region($this->asiaComplete);
+		return $this->getRegion($this->asiaCompleteIso3, $as);
 	}
 
 	/**
+	 * @param int $as
 	 * @return array
      */
-	public function get_oceania()
+	public function getOceania($as = self::RETURN_AS_ISO3)
 	{
-		return $this->get_region($this->oceaniaComplete);
+		return $this->getRegion($this->oceaniaCompleteIso3, $as);
+	}
+
+	/**
+	 * @param string $code
+	 * @return string
+	 * @throws Exception
+	 */
+	public function convertIso2($code)
+	{
+		if (array_key_exists($code, $this->iso2ToIso3)) {
+			return $this->iso2ToIso3[$code];
+		}
+		throw new Exception(sprintf('Mapping not found for ISO2 code %s', $code));
+	}
+
+	/**
+	 * @param string $code
+	 * @return string
+	 * @throws Exception
+	 */
+	public function convertIso3($code)
+	{
+		static $iso3ToIso2 = null;
+		if (is_null($iso3ToIso2)) {
+			$iso3ToIso2 = array_flip($this->iso2ToIso3);
+		}
+		if (array_key_exists($code, $iso3ToIso2)) {
+			return $iso3ToIso2[$code];
+		}
+		throw new Exception(sprintf('Mapping not found for ISO3 code %s', $code));
 	}
 
 	/**
 	 * @param array $region
+	 * @param int $as
 	 * @return array
 	 */
-	protected function get_region(array $region)
+	protected function getRegion(array $region, $as = self::RETURN_AS_ISO3)
 	{
-		$this->exclude = array();
-		foreach (array_keys($this->complete) as $current) {
+		$this->exclude = [];
+		foreach (array_keys($this->completeIso3) as $current) {
 			if (!in_array($current, $region)) {
 				$this->exclude[$current] = $current;
 			}
 		}
-		return $this->get();
+		return $this->get($as);
+	}
+
+	/**
+	 * @param array $list
+	 * @return array
+	 * @throws Exception
+     */
+	protected function convertListToIso2(array $list)
+	{
+		$iso2List = [];
+		foreach ($list as $code => $name) {
+			$iso2List[$this->convertIso3($code)] = $name;
+		}
+		return $iso2List;
 	}
 }
